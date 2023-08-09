d3e2997143a9e7b30f97270ad4cc0dcc59354f74
General Jack O'Neill <68293825+StarGate-One@users.noreply.github.com>
2023-08-08 22:59:44 -0400

    Add Create Database, Fix Syntax in select-en-kjv.sql

--- select-en-kjv.sql
+++ select-en-kjv.sql
@@ -1,10 +1,10 @@
 use ChristianBible;
 
-/* select * from en_kjv;
+/*select * from dbo.en_kjv;
   where canon_book = 'REV' OR canon_book = 'GEN';
    order by canon_chapter, canon_start_verse; */
 
-/* select distinct canon_book, substring(canon_order,1,3) from en_kjv
+ /*select distinct canon_book, substring(canon_order,1,3) from dbo.en_kjv
    order by substring(canon_order,1,3); */
 
-select * from dbo.en_bible;
+ select * from dbo.en_bible; 
